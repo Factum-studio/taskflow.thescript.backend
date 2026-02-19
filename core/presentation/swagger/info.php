345ab70b5e379826bb019032ea18d8a1a3361960
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-02-19 16:29:09 +0500

    #3 add project info
    Добавляем информацию о проекте для сваггер

---
+++ core/presentation/swagger/info.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @OA\Info(
+ *     title="TASKFLOW CRM API",
+ *     version="1.0.0",
+ *     description="CRM API for TASKFLOW project",
+ *     @OA\Contact(
+ *         email="support@example.com"
+ *     )
+ * )
+ * @OA\Server(
+ *     url="http://localhost:8080",
+ *     description="API dev server"
+ * )
+ * @OA\Server(
+ *     url="https://api.taskflow.thescript.agency",
+ *     description="API prod server"
+ * )
+ * @OA\OpenApi(
+ *     openapi="3.0.0"
+ * )
+ */
+class InfoDefinitions {}
