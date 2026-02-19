71677c3d9a6f9af5d9d26d5fc4dcd84abc88b9f3
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-02-19 22:34:15 +0500

    #9 fix success dto
    - добавлено отображение если не пришёл null
    - добавлено отображение если пришёл массив
    - добавлено отображение если пришёл другой объект с jsonSerialize
    - добавлено отображение для скаляров

--- core/application/dto/SuccessDto.php
+++ core/application/dto/SuccessDto.php
@@ -11,9 +11,13 @@ class SuccessDto implements \JsonSerializable
 
     public function jsonSerialize(): array
     {
-        if ($this->data === null) {
-            return ['message' => $this->message];
-        }
-        return $this->data;
+        if ($this->data === null) return ['message' => $this->message];
+
+        if (is_array($this->data)) return $this->data;
+
+        if (is_object($this->data) && method_exists($this->data, 'jsonSerialize'))
+            return $this->data->jsonSerialize();
+
+        return ['data' => $this->data];
     }
 }
