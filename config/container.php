289de8d34ec5d390e6c894bdc232133a2bb7965d
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-02-22 03:24:09 +0500

    #21 update container

--- config/container.php
+++ config/container.php
@@ -1,9 +1,18 @@
 <?php
 
+use core\application\port\IPassportGateway;
 use core\application\port\IJwtValidator;
+use core\infrastructure\passport\HttpPassportGateway;
 use core\infrastructure\jwt\JwtValidator;
+use yii\httpclient\Client;
 
 $container = Yii::$container;
+
+$container->set(IPassportGateway::class, function () {
+    return new HttpPassportGateway(
+        Yii::$app->get('passportHttpClient')
+    );
+});
 
 $container->set(IJwtValidator::class, function () {
     $config = Yii::$app->params['jwt'];
