687be551a7003fe00d8b02a9b190dcc685db3a6d
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-02-23 20:59:50 +0500

    #21 update container config

--- config/container.php
+++ config/container.php
@@ -2,15 +2,35 @@
 
 use core\application\port\IPassportGateway;
 use core\application\port\IJwtValidator;
+use core\application\port\IUrlBuilder;
+use core\infrastructure\http\ApiUrlBuilder;
+use core\infrastructure\http\config\ApiEndpointConfig;
 use core\infrastructure\passport\HttpPassportGateway;
 use core\infrastructure\jwt\JwtValidator;
-use yii\httpclient\Client;
 
 $container = Yii::$container;
 
-$container->set(IPassportGateway::class, function () {
+$container->setSingleton(ApiEndpointConfig::class, function() {
+    return new ApiEndpointConfig([
+        'user' => 'user',
+        'contact' => 'contact',
+        'city' => 'city',
+        'post' => 'post',
+    ]);
+});
+
+$container->setSingleton(IUrlBuilder::class, function() use ($container) {
+    $baseUrl = $_ENV['USER_SERVICE_BASE_URL'] ?? '';
+
+    return (new ApiUrlBuilder($baseUrl))
+        ->withVersion('v1');
+});
+
+$container->setSingleton(IPassportGateway::class, function() use ($container) {
     return new HttpPassportGateway(
-        Yii::$app->get('passportHttpClient')
+        Yii::$app->passportHttpClient,
+        $container->get(IUrlBuilder::class),
+        $container->get(ApiEndpointConfig::class)
     );
 });
 
