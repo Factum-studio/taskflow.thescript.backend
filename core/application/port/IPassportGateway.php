3889193471c6ff27cacb025cab9e149588437dc3
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-02-23 21:00:46 +0500

    #21 add more breaks to work
    Добавлены новые брейкпоинты для работы с passport
    user
    contacts
    city
    post

--- core/application/port/IPassportGateway.php
+++ core/application/port/IPassportGateway.php
@@ -2,10 +2,13 @@
 
 namespace core\application\port;
 
-use core\application\dto\CollectionDto;
 use core\domain\valueObject\JwtToken;
+use core\domain\valueObject\QueryParams;
 
 interface IPassportGateway
 {
-    public function getUserData(JwtToken $token): array;
+    public function getUserData(JwtToken $token, ?QueryParams $params = null): array;
+    public function getContactData(JwtToken $token, ?QueryParams $params = null): array;
+    public function getCityData(JwtToken $token, ?QueryParams $params = null): array;
+    public function getPostData(JwtToken $token, ?QueryParams $params = null): array;
 }
