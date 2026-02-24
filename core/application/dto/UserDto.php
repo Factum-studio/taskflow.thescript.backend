1994fdadb054b19a118202292b978dc3eaec4e7e
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-02-24 07:18:48 +0500

    #21 add user dto

---
+++ core/application/dto/UserDto.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace core\application\dto;
+
+use core\domain\entity\User;
+use core\domain\valueObject\Contact;
+
+class UserDto implements \JsonSerializable
+{
+    private function __construct(
+        private readonly array $data
+    ) {}
+
+    public static function fromEntity(User $user): self
+    {
+        $contacts = array_map(
+            fn(Contact $contact) => [
+                'type' => $contact->getType(),
+                'value' => $contact->getValue(),
+                'confirmed' => $contact->isConfirmed(),
+            ],
+            $user->getContacts()
+        );
+
+        $post = $user->getPost() ? [
+            'id' => $user->getPost()->getId(),
+            'name' => $user->getPost()->getName(),
+        ] : null;
+
+        return new self([
+            'id' => $user->getId()->value(),
+            'surname' => $user->getSurname(),
+            'name' => $user->getName(),
+            'patronymic' => $user->getPatronymic(),
+            'ui_name' => $user->getUiName(),
+            'dob' => $user->getDob(),
+            'contacts' => $contacts,
+            'post' => $post,
+        ]);
+    }
+
+    public function jsonSerialize(): array
+    {
+        return $this->data;
+    }
+
+    public function toArray(): array
+    {
+        return $this->data;
+    }
+}
