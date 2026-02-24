0717961aa376a0fbf858a4101f74b48ad40eb3fb
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-02-24 07:27:42 +0500

    #21 update di config
    Инициализированы необходимые зависимости с контейнере

--- config/container.php
+++ config/container.php
@@ -1,12 +1,19 @@
 <?php
 
+use core\application\handler\GetCurrentUserQueryHandler;
 use core\application\port\IPassportGateway;
 use core\application\port\IJwtValidator;
 use core\application\port\IUrlBuilder;
+use core\application\port\IUserRepository;
+use core\application\useCase\AuthenticateByJwtUseCase;
+use core\application\useCase\GetAuthenticatedUserUseCase;
 use core\infrastructure\http\ApiUrlBuilder;
 use core\infrastructure\http\config\ApiEndpointConfig;
 use core\infrastructure\passport\HttpPassportGateway;
 use core\infrastructure\jwt\JwtValidator;
+use core\infrastructure\repository\PassportUserRepository;
+use core\presentation\controller\UserController;
+use core\security\JwtMiddleware;
 
 $container = Yii::$container;
 
@@ -21,9 +28,7 @@ $container->setSingleton(ApiEndpointConfig::class, function() {
 
 $container->setSingleton(IUrlBuilder::class, function() use ($container) {
     $baseUrl = $_ENV['USER_SERVICE_BASE_URL'] ?? '';
-
-    return (new ApiUrlBuilder($baseUrl))
-        ->withVersion('v1');
+    return (new ApiUrlBuilder($baseUrl))->withVersion('v1');
 });
 
 $container->setSingleton(IPassportGateway::class, function() use ($container) {
@@ -43,3 +48,32 @@ $container->set(IJwtValidator::class, function () {
         $config['audience']
     );
 });
+
+$container->setSingleton(AuthenticateByJwtUseCase::class, function() use ($container) {
+    return new AuthenticateByJwtUseCase(
+        $container->get(IJwtValidator::class)
+    );
+});
+
+$container->setSingleton(IUserRepository::class, function() use ($container) {
+    return new PassportUserRepository(
+        $container->get(IPassportGateway::class),
+        Yii::$app->cache,
+        3600
+    );
+});
+
+$container->setSingleton(GetAuthenticatedUserUseCase::class, function() use ($container) {
+    return new GetAuthenticatedUserUseCase(
+        $container->get(AuthenticateByJwtUseCase::class),
+        $container->get(IUserRepository::class)
+    );
+});
+
+$container->setSingleton(JwtMiddleware::class, function() use ($container) {
+    return new JwtMiddleware(
+        $container->get(GetAuthenticatedUserUseCase::class)
+    );
+});
+
+$container->setSingleton(GetCurrentUserQueryHandler::class);
