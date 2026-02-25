16b30c2a6698b7ea08df225d87a4853f9f2481df
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-02-25 09:10:59 +0500

    #27 inc migration namespace in config

--- config/migration_namespaces.php
+++ config/migration_namespaces.php
@@ -8,7 +8,7 @@ return [
 //    'core\infrastructure\migrations',
 //    'modules\clients\infrastructure\migrations',
 //    'modules\projects\infrastructure\migrations',
-//    'modules\tasks\infrastructure\migrations',
+    'modules\tasks\infrastructure\migrations',
 //    'modules\kanban\infrastructure\migrations',
 //    'modules\files\infrastructure\migrations',
 //    'modules\analytics\infrastructure\migrations',
