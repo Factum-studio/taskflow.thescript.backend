3801a3f59f094272c9eb9db3afcb54631ac2e56f
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-02-25 09:43:49 +0500

    #27 add di config

---
+++ modules/tasks/config/di.php
@@ -0,0 +1,52 @@
+<?php
+
+use modules\tasks\domain\event\TaskRestoredEvent;
+use modules\tasks\domain\event\TaskSoftDeletedEvent;
+use modules\tasks\domain\event\TaskUpdatedEvent;
+use modules\tasks\domain\repository\ITaskRepository;
+use modules\tasks\domain\event\IEventDispatcher;
+use modules\tasks\infrastructure\repository\DbTaskRepository;
+use modules\tasks\infrastructure\event\ModuleEventDispatcher;
+use modules\tasks\infrastructure\listener\TaskLoggerListener;
+use modules\tasks\infrastructure\listener\TaskNotificationListener;
+use modules\tasks\domain\event\TaskCreatedEvent;
+use modules\tasks\domain\event\TaskStatusChangedEvent;
+use modules\tasks\domain\event\TaskAssignedEvent;
+use yii\di\Container;
+
+Yii::$container->set(ITaskRepository::class, DbTaskRepository::class);
+
+Yii::$container->set(TaskLoggerListener::class);
+Yii::$container->set(TaskNotificationListener::class);
+
+Yii::$container->set(IEventDispatcher::class, function (Container $container) {
+    /** @var TaskLoggerListener $logger */
+    $logger = $container->get(TaskLoggerListener::class);
+    /** @var TaskNotificationListener $notifier */
+    $notifier = $container->get(TaskNotificationListener::class);
+
+    $listeners = [
+        TaskCreatedEvent::class => [
+            [$logger, 'handleTaskCreated'],
+        ],
+        TaskStatusChangedEvent::class => [
+            [$logger, 'handleTaskStatusChanged'],
+            [$notifier, 'handleTaskStatusChanged'],
+        ],
+        TaskAssignedEvent::class => [
+            [$logger, 'handleTaskAssigned'],
+            [$notifier, 'handleTaskAssigned'],
+        ],
+        TaskRestoredEvent::class => [
+            [$logger, 'handleTaskRestored'],
+        ],
+        TaskSoftDeletedEvent::class => [
+            [$logger, 'handleTaskSoftDeleted'],
+        ],
+        TaskUpdatedEvent::class => [
+            [$logger, 'handleTaskUpdated'],
+        ]
+    ];
+
+    return new ModuleEventDispatcher($listeners);
+});
