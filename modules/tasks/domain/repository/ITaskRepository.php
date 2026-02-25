095179c396f984f1961a8439b9d1757a71233c7a
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-02-25 09:17:58 +0500

    #27 add task repository

---
+++ modules/tasks/domain/repository/ITaskRepository.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace modules\tasks\domain\repository;
+
+use modules\tasks\domain\entity\Task;
+use modules\tasks\domain\valueObject\TaskId;
+
+interface ITaskRepository
+{
+    public function save(Task $task): Task;
+    public function findById(TaskId $id): ?Task;
+    /**
+     * @param array $criteria
+     * @return Task[]
+     */
+    public function findAll(array $criteria = []): array;
+    public function remove(Task $task): void;
+}
