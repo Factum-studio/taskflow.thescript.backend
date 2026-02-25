2fcafad6e56c5f325bfadeb55461d9f34560275b
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-02-25 09:47:03 +0500

    #27 add requests
    Добавлены запросы для:
    - Мягкого удаления
    - физического удаления
    - назначения исполнителя на задачу
    - изменения статуса
    - создание задачи
    - восстановления после мягкого удаления
    - обновления задачи

---
+++ modules/tasks/presentation/request/HardDeleteTaskRequest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace modules\tasks\presentation\request;
+
+use yii\base\Model;
+
+class HardDeleteTaskRequest extends Model
+{
+    // А ты уже видел невообразимые внутренности RestoreTaskRequest и SoftDeleteTaskRequest? Посмотри, посмотри, тебе точно будет интересно ♥
+}
---
+++ modules/tasks/presentation/request/RestoreTaskRequest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace modules\tasks\presentation\request;
+
+use yii\base\Model;
+
+class RestoreTaskRequest extends Model
+{
+    // Ты как? Давно не виделись) Ты сюда из какого файла пришёл? Если из SoftDeleteTaskRequest, то я с тебя просто в шоке...
+}
