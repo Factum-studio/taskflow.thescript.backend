969b192d7d3d59625a27a3d915b67215f1597fdf
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-02-26 10:03:57 +0500

    #27 update create task handler
    Обновляем хэндлер создания задачи для проверки существования статуса и приоритета

--- modules/tasks/application/handler/CreateTaskHandler.php
+++ modules/tasks/application/handler/CreateTaskHandler.php
@@ -2,13 +2,16 @@
 
 namespace modules\tasks\application\handler;
 
+use InvalidArgumentException;
 use modules\tasks\application\assembler\TaskDtoAssembler;
 use modules\tasks\application\command\CreateTaskCommand;
 use modules\tasks\application\dto\TaskDto;
 use modules\tasks\domain\entity\Task;
 use modules\tasks\domain\event\IEventDispatcher;
 use modules\tasks\domain\event\TaskCreatedEvent;
+use modules\tasks\domain\repository\ITaskPriorityRepository;
 use modules\tasks\domain\repository\ITaskRepository;
+use modules\tasks\domain\repository\ITaskStatusRepository;
 use modules\tasks\domain\valueObject\PriorityId;
 use modules\tasks\domain\valueObject\StatusId;
 use modules\tasks\domain\valueObject\TaskId;
@@ -21,22 +24,36 @@ class CreateTaskHandler
     private ITaskRepository $taskRepository;
     private TaskDtoAssembler $taskDtoAssembler;
     private IEventDispatcher $eventDispatcher;
+    private ITaskStatusRepository $statusRepository;
+    private ITaskPriorityRepository $priorityRepository;
 
     public function __construct(
         ITaskRepository $taskRepository,
         TaskDtoAssembler $taskDtoAssembler,
-        IEventDispatcher $eventDispatcher
+        IEventDispatcher $eventDispatcher,
+        ITaskStatusRepository $statusRepository,
+        ITaskPriorityRepository $priorityRepository
     ) {
-        $this->taskRepository   = $taskRepository;
-        $this->taskDtoAssembler = $taskDtoAssembler;
-        $this->eventDispatcher  = $eventDispatcher;
+        $this->taskRepository       = $taskRepository;
+        $this->taskDtoAssembler     = $taskDtoAssembler;
+        $this->eventDispatcher      = $eventDispatcher;
+        $this->statusRepository     = $statusRepository;
+        $this->priorityRepository   = $priorityRepository;
     }
 
     public function handle(CreateTaskCommand $command): TaskDto
     {
-        $title      = new Title($command->title);
-        $statusId   = new StatusId($command->statusId);
+        $statusId = new StatusId($command->statusId);
+        if (!$this->statusRepository->findById($statusId)) {
+            throw new InvalidArgumentException("Status with ID {$command->statusId} does not exist.");
+        }
+
         $priorityId = new PriorityId($command->priorityId);
+        if (!$this->priorityRepository->findById($priorityId)) {
+            throw new InvalidArgumentException("Priority with ID {$command->priorityId} does not exist.");
+        }
+
+        $title      = new Title($command->title);
         $createdBy  = new UserId($command->createdBy);
         $assignedTo = $command->assignedTo ? new UserId($command->assignedTo) : null;
         $parentId   = $command->parentId ? new TaskId($command->parentId) : null;
