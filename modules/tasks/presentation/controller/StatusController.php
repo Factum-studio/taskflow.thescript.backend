f2cfd7fa3427a78d2af2f2bf846fbef9ed640dee
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-02-26 09:56:20 +0500

    #27 add swagger routs to controllers

--- modules/tasks/presentation/controller/StatusController.php
+++ modules/tasks/presentation/controller/StatusController.php
@@ -6,7 +6,12 @@ use core\application\dto\CollectionDto;
 use core\presentation\controller\BaseController;
 use modules\tasks\application\handler\ListStatusesHandler;
 use modules\tasks\application\query\ListStatusesQuery;
+use OpenApi\Attributes as OA;
 
+#[OA\Tag(
+    name: 'task-statuses',
+    description: 'Справочник статусов задач'
+)]
 class StatusController extends BaseController
 {
     public function __construct(
@@ -18,6 +23,33 @@ class StatusController extends BaseController
         parent::__construct($id, $module, $config);
     }
 
+    #[OA\Get(
+        path: '/task-status',
+        summary: 'Список статусов задач',
+        security: [['bearerAuth' => []]],
+        tags: ['task-statuses'],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: 'Успешный ответ',
+                content: new OA\JsonContent(
+                    properties: [
+                        new OA\Property(
+                            property: 'items',
+                            type: 'array',
+                            items: new OA\Items(ref: '#/components/schemas/TaskStatus')
+                        ),
+                        new OA\Property(
+                            property: '_meta',
+                            ref: '#/components/schemas/Collection/properties/_meta'
+                        )
+                    ],
+                    type: 'object'
+                )
+            ),
+            new OA\Response(response: 401, description: 'Требуется авторизация')
+        ]
+    )]
     public function actionIndex(): CollectionDto|array
     {
         $query = new ListStatusesQuery();
