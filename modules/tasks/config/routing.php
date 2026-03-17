9b290050056eaa2aae3631bb13d1056532658118
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-03-17 18:14:50 +0500

    #27 update routing
    Меняем роутинг, для работы с колонками

--- modules/tasks/config/routing.php
+++ modules/tasks/config/routing.php
@@ -6,12 +6,10 @@ return [
     'POST task' => 'tasks/task/create',
     'PUT task/<id:\d+>' => 'tasks/task/update',
     'DELETE task/<id:\d+>' => 'tasks/task/delete',
-    'POST task/<id:\d+>/change-status' => 'tasks/task/change-status',
+    'POST task/<id:\d+>/move-to-column' => 'tasks/task/move-to-column',
     'POST task/<id:\d+>/assign' => 'tasks/task/assign',
     'POST task/<id:\d+>/restore' => 'tasks/task/restore',
     'DELETE task/<id:\d+>/hard' => 'tasks/task/hard-delete',
-
-    'GET task-status' => 'tasks/status/index',
 
     'GET task-priority' => 'tasks/priority/index',
 
