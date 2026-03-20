36679812780d006328a0dffad39fb6739909190a
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-03-20 11:06:48 +0500

    #45 style update

--- modules/projects/infrastructure/repository/DbBoardRepository.php
+++ modules/projects/infrastructure/repository/DbBoardRepository.php
@@ -25,6 +25,9 @@ class DbBoardRepository implements IBoardRepository
         $this->db = $db;
     }
 
+    /**
+     * @throws \yii\db\Exception
+     */
     public function save(Board $board): Board
     {
         $ar = $this->findARById($board->getId()) ?? new BoardAR();
@@ -41,6 +44,9 @@ class DbBoardRepository implements IBoardRepository
         return $board;
     }
 
+    /**
+     * @throws Exception
+     */
     public function findById(BoardId $id): ?Board
     {
         $ar = $this->findARById($id);
@@ -73,11 +79,11 @@ class DbBoardRepository implements IBoardRepository
 
     private function mapEntityToAR(Board $board, BoardAR $ar): void
     {
-        $ar->project_id = $board->getProjectId()->getValue();
-        $ar->name = $board->getName();
-        $ar->description = $board->getDescription();
-        $ar->created_by = $board->getCreatedBy()->getValue();
-        $ar->settings = $board->getSettings()->toArray();
+        $ar->project_id     = $board->getProjectId()->getValue();
+        $ar->name           = $board->getName();
+        $ar->description    = $board->getDescription();
+        $ar->created_by     = $board->getCreatedBy()->getValue();
+        $ar->settings       = $board->getSettings()->toArray();
     }
 
     /**
