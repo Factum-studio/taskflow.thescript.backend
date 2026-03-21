02162a47f2beb686bcf41fe04b688e38d678d979
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-03-21 18:02:08 +0500

    #45 add check access for get project

--- modules/projects/application/handler/GetProjectHandler.php
+++ modules/projects/application/handler/GetProjectHandler.php
@@ -4,6 +4,7 @@ namespace modules\projects\application\handler;
 
 use modules\projects\application\assembler\ProjectDtoAssembler;
 use modules\projects\application\dto\ProjectDto;
+use modules\projects\application\port\IProjectAccess;
 use modules\projects\application\query\GetProjectQuery;
 use modules\projects\domain\repository\IProjectRepository;
 use modules\projects\domain\valueObject\ProjectId;
@@ -13,13 +14,16 @@ class GetProjectHandler
 {
     private IProjectRepository $projectRepository;
     private ProjectDtoAssembler $projectDtoAssembler;
+    private IProjectAccess $projectAccess;
 
     public function __construct(
         IProjectRepository $projectRepository,
-        ProjectDtoAssembler $projectDtoAssembler
+        ProjectDtoAssembler $projectDtoAssembler,
+        IProjectAccess $projectAccess
     ) {
-        $this->projectRepository = $projectRepository;
-        $this->projectDtoAssembler = $projectDtoAssembler;
+        $this->projectRepository    = $projectRepository;
+        $this->projectDtoAssembler  = $projectDtoAssembler;
+        $this->projectAccess        = $projectAccess;
     }
 
     public function handle(GetProjectQuery $query): ProjectDto
@@ -29,6 +33,10 @@ class GetProjectHandler
 
         if ($project === null) {
             throw new RuntimeException("Project with ID {$query->id} not found");
+        }
+
+        if (!$this->projectAccess->canViewProject($query->userId, $query->id)) {
+            throw new RuntimeException('You are not allowed to view this project');
         }
 
         return $this->projectDtoAssembler->toDto($project);
