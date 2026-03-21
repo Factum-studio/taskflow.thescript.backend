6ca06a353cea673131228a51a5c49ee2c8d9e1da
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-03-21 18:02:37 +0500

    #45 add check access for get project boards

--- modules/projects/application/handler/ListProjectBoardsHandler.php
+++ modules/projects/application/handler/ListProjectBoardsHandler.php
@@ -4,21 +4,26 @@ namespace modules\projects\application\handler;
 
 use modules\projects\application\assembler\BoardDtoAssembler;
 use modules\projects\application\dto\BoardDto;
+use modules\projects\application\port\IProjectAccess;
 use modules\projects\application\query\ListProjectBoardsQuery;
 use modules\projects\domain\repository\IBoardRepository;
 use modules\projects\domain\valueObject\ProjectId;
+use RuntimeException;
 
 class ListProjectBoardsHandler
 {
     private IBoardRepository $boardRepository;
     private BoardDtoAssembler $boardDtoAssembler;
+    private IProjectAccess $projectAccess;
 
     public function __construct(
         IBoardRepository $boardRepository,
-        BoardDtoAssembler $boardDtoAssembler
+        BoardDtoAssembler $boardDtoAssembler,
+        IProjectAccess $projectAccess
     ) {
-        $this->boardRepository = $boardRepository;
-        $this->boardDtoAssembler = $boardDtoAssembler;
+        $this->boardRepository      = $boardRepository;
+        $this->boardDtoAssembler    = $boardDtoAssembler;
+        $this->projectAccess        = $projectAccess;
     }
 
     /**
@@ -26,6 +31,10 @@ class ListProjectBoardsHandler
      */
     public function handle(ListProjectBoardsQuery $query): array
     {
+        if (!$this->projectAccess->canViewProject($query->userId, $query->projectId)) {
+            throw new RuntimeException('You are not allowed to view boards of this project');
+        }
+
         $boards = $this->boardRepository->findByProject(new ProjectId($query->projectId));
         return $this->boardDtoAssembler->toDtoList($boards);
     }
