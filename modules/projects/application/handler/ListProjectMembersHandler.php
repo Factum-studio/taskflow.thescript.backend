9079a1036f82437b51de9b9d7958ceb4b74363b0
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-03-21 18:02:46 +0500

    #45 add check access for get project members

--- modules/projects/application/handler/ListProjectMembersHandler.php
+++ modules/projects/application/handler/ListProjectMembersHandler.php
@@ -4,21 +4,26 @@ namespace modules\projects\application\handler;
 
 use modules\projects\application\assembler\ProjectUserDtoAssembler;
 use modules\projects\application\dto\ProjectUserDto;
+use modules\projects\application\port\IProjectAccess;
 use modules\projects\application\query\ListProjectMembersQuery;
 use modules\projects\domain\repository\IProjectUserRepository;
 use modules\projects\domain\valueObject\ProjectId;
+use RuntimeException;
 
 class ListProjectMembersHandler
 {
     private IProjectUserRepository $projectUserRepository;
     private ProjectUserDtoAssembler $projectUserDtoAssembler;
+    private IProjectAccess $projectAccess;
 
     public function __construct(
         IProjectUserRepository $projectUserRepository,
-        ProjectUserDtoAssembler $projectUserDtoAssembler
+        ProjectUserDtoAssembler $projectUserDtoAssembler,
+        IProjectAccess $projectAccess
     ) {
-        $this->projectUserRepository = $projectUserRepository;
-        $this->projectUserDtoAssembler = $projectUserDtoAssembler;
+        $this->projectUserRepository    = $projectUserRepository;
+        $this->projectUserDtoAssembler  = $projectUserDtoAssembler;
+        $this->projectAccess            = $projectAccess;
     }
 
     /**
@@ -26,6 +31,10 @@ class ListProjectMembersHandler
      */
     public function handle(ListProjectMembersQuery $query): array
     {
+        if (!$this->projectAccess->canViewProject($query->userId, $query->projectId)) {
+            throw new RuntimeException('You are not allowed to view members of this project');
+        }
+
         $members = $this->projectUserRepository->findByProject(new ProjectId($query->projectId));
         return $this->projectUserDtoAssembler->toDtoList($members);
     }
