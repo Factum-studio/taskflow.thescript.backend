4106ae75022e8e46ff7acac7b87127589f981999
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-03-21 15:29:04 +0500

    #45 add access check for change member role

--- modules/projects/application/handler/ChangeMemberRoleHandler.php
+++ modules/projects/application/handler/ChangeMemberRoleHandler.php
@@ -5,6 +5,7 @@ namespace modules\projects\application\handler;
 use modules\projects\application\assembler\ProjectUserDtoAssembler;
 use modules\projects\application\command\ChangeMemberRoleCommand;
 use modules\projects\application\dto\ProjectUserDto;
+use modules\projects\application\port\IProjectAccess;
 use modules\projects\domain\repository\IProjectUserRepository;
 use modules\projects\domain\valueObject\ProjectId;
 use modules\projects\domain\valueObject\UserId;
@@ -15,13 +16,16 @@ class ChangeMemberRoleHandler
 {
     private IProjectUserRepository $projectUserRepository;
     private ProjectUserDtoAssembler $projectUserDtoAssembler;
+    private IProjectAccess $projectAccess;
 
     public function __construct(
         IProjectUserRepository $projectUserRepository,
-        ProjectUserDtoAssembler $projectUserDtoAssembler
+        ProjectUserDtoAssembler $projectUserDtoAssembler,
+        IProjectAccess $projectAccess
     ) {
         $this->projectUserRepository    = $projectUserRepository;
         $this->projectUserDtoAssembler  = $projectUserDtoAssembler;
+        $this->projectAccess            = $projectAccess;
     }
 
     public function handle(ChangeMemberRoleCommand $command): ProjectUserDto
@@ -29,16 +33,16 @@ class ChangeMemberRoleHandler
         $projectId = new ProjectId($command->projectId);
         $userId = new UserId($command->userId);
 
+        if (!$this->projectAccess->canManageUser($command->changedBy, $command->userId, $command->projectId)) {
+            throw new RuntimeException('You are not allowed to change this user\'s role');
+        }
+
         $projectUser = $this->projectUserRepository->find($projectId, $userId);
         if ($projectUser === null) {
             throw new RuntimeException("User is not a member of this project");
         }
 
-        // Проверка прав: админ проекта может менять роли
-        // TODO: проверить, что $command->changedBy является админом
-
         $projectUser->changeRole(new UserRole($command->newRole));
-
         $this->projectUserRepository->save($projectUser);
 
         return $this->projectUserDtoAssembler->toDto($projectUser);
