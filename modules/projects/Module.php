7b3c5d79e2e7f237df923b0f955747f2e9989b5c
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-03-31 03:59:58 +0500

    #53 fix di in prj inject
    Реализовано централизованное подключение di модулей

--- modules/projects/Module.php
+++ modules/projects/Module.php
@@ -6,20 +6,4 @@ use yii\base\Module as BaseModule;
 
 class Module extends BaseModule
 {
-    public function init(): void
-    {
-        parent::init();
-        $this->loadDiConfig();
-    }
-
-    /**
-     * Загружает конфигурацию DI-контейнера модуля
-     */
-    private function loadDiConfig(): void
-    {
-        $diConfig = __DIR__ . '/config/di.php';
-        if (file_exists($diConfig)) {
-            require $diConfig;
-        }
-    }
 }
